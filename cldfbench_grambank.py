5ed63f58635a41e5686f24c9dee4051cdc169ef9
Johannes Englisch <johannes_englisch@eva.mpg.de>
2024-05-17 16:21:12 +0200

    retrieve mergeable values from unmerged conflicts

--- cldfbench_grambank.py
+++ cldfbench_grambank.py
@@ -1,5 +1,5 @@
 import collections
-from itertools import islice
+from itertools import chain, islice
 import pathlib
 import re
 import shutil
@@ -7,9 +7,10 @@ import subprocess
 
 from clldutils.markup import iter_markdown_tables
 from cldfbench import Dataset as BaseDataset, CLDFSpec
+from csvw import dsv
 from pygrambank import Grambank
 from pygrambank.sheet import Sheet
-from pygrambank.util import iterunique
+from pygrambank.util import iterunique, adhoc_merge
 from pygrambank.contributors import PHOTO_URI, ROLES
 
 from pygrambank.cldf import GlottologGB, BibliographyMatcher
@@ -184,6 +185,15 @@ class Dataset(BaseDataset):
         # Chose best sheet for indivdual Glottocodes:
         print('selecting best sheets')
         sheets = list(iterunique(sheets, verbose=True))
+
+        # Add values from conflict sheets
+        conflict_sheets = (
+            (conflict.stem, list(dsv.reader(conflict, delimiter='\t', dicts=True, encoding='utf-8')))
+            for conflict in grambank.path('conflicts').glob('*.tsv'))
+        sheets.extend(
+            pair
+            for glottocode, conflict_rows in conflict_sheets
+            if (pair := adhoc_merge(grambank, glottocode, conflict_rows)))
 
         print('collecting data points in data sheets')
 
